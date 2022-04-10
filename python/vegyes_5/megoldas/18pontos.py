16249ebb745a219559a546e46f3a4db1fa514b20
molnarjEtvs <molnarj@elg-bp.edu.hu>
2022-04-10 20:50:17 +0200

    rossz helyen volt a 8pontos

--- python/vegyes_5/megoldas/18pontos.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('cls')
-szel=int(input("Add meg a szoba szélességét méterben: "))
-hosz=int(input("Add meg a szoba hosszúságát méterben: "))
-nmAr=int(input("Add meg a burkolás egy m2 árát forintban: "))
-nm = (szel*hosz)
-ar=nm*nmAr*1.1
-print("A szoba "+str(nm)+"m2, burkolási költsége: "+str(ar)+" Ft")
